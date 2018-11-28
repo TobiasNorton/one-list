0fac37eb59e2b604cc42bc6419a428ca14508182
Tobias Norton <tnorton20@yahoo.com>
2018-11-28 13:34:50 -0500

    Adjusted spacing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,82 @@ import './App.css'
 import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [],
+      newItemText: ''
+    }
+  }
+
+  loadAllTodos = () => {
+    axios
+      .get('https://one-list-api.herokuapp.com/items?access_token=wagyu_wednesday')
+      .then(response => {
+        this.setState({
+          todos: response.data
+        })
+      })
+  }
+
+  componentDidMount = () => {
+    this.loadAllTodos()
+  }
+
+  changeText = event => {
+    this.setState(
+      {
+        newItemText: event.target.value
+      },
+      () => console.log(this.state.newItemText)
+    )
+  }
+
+  newItem = event => {
+    event.preventDefault()
+    axios
+      .post('https://one-list-api.herokuapp.com/items?access_token=wagyu_wednesday', {
+        item: {
+          text: this.state.newItemText,
+          complete: false
+        }
+      })
+      .then(response => {
+        this.loadAllTodos()
+      })
+    console.log(this.state.newItemText)
+  }
+
+  completeTodo = event => {
+    axios
+      .put(
+        `https://one-list-api.herokuapp.com/items/${
+          event.target.dataset.idfromapi
+        }?access_token=wagyu_wednesday`,
+        {
+          item: {
+            complete: true
+          }
+        }
+      )
+      .then(response => {
+        this.loadAllTodos()
+      })
+  }
+
+  deleteItem = event => {
+    axios
+      .delete(
+        `https://one-list-api.herokuapp.com/items/${
+          event.target.dataset.idfromapi
+        }?access_token=wagyu_wednesday`
+      )
+      .then(response => {
+        this.loadAllTodos()
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,20 +87,36 @@ class App extends Component {
           <h1>One List</h1>
         </header>
         <main>
+          <form onSubmit={this.newItem}>
+            <input
+              value={this.state.newItemText}
+              type="text"
+              placeholder=" Whats up?"
+              onChange={this.changeText}
+            />
+          </form>
           <ul className="one-list">
-            <li className="completed">Review at 2</li>
-            <li>Watch Videos</li>
-            <li>Eat Dinner</li>
-            <li>Code</li>
-            <li>Sleep</li>
+            {this.state.todos.map((todo, index) => {
+              const todoClass = todo.complete ? 'completed' : ''
+              return (
+                <li
+                  key={index}
+                  data-idfromapi={todo.id} //this is the actual id key from the object
+                  onClick={this.completeTodo}
+                  onDoubleClick={this.deleteItem}
+                  className={todoClass}
+                >
+                  {todo.text}
+                </li>
+              )
+            })}
           </ul>
-          <input type="text" placeholder="Whats up?" />
         </main>
         <footer>
           <p>
             <img src={logo} height="42" alt="logo" />
           </p>
-          <p>&copy; 2018 Tobias Norton Productivity Advocates</p>
+          <p>&copy; Tobias Norton Productivity Advocates</p>
         </footer>
       </div>
     )
